1c129ea27e9fb473cf3ca50eeb96f53ae3dc089b
bloody <127643143+bloodymademe@users.noreply.github.com>
2023-03-12 14:17:30 +0000

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,3 +1,15 @@
+getgenv().Key = "BANME"
+
+local key = getgenv().Key
+local check = "https://bloodymade.000webhostapp.com/check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+    if syn then
+        printconsole("WHITELISTED")
+    end
+else
+    game.Players.LocalPlayer:Kick("YOU'RE ON THE BLACKLIST SECTION, DM joka#1337 TO APPEAL YOURSELF.")
+end
+
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
 	ThemeManager.Folder = 'LinoriaLibSettings'
