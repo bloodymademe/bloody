06bce452cf2a2c12f484c6c413cbb4e297b35ff5
bloody <127643143+bloodymademe@users.noreply.github.com>
2023-03-12 13:23:39 +0000

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,3 +1,14 @@
+get.genv().Key = "BLAST"
+
+local key = _G.Key
+local check = "https://lepidote-vibration.000webhostapp.com/check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+	printconsole("wl")
+end
+else
+	game.Players.LocalPlayer:Kick("YOURE BLACKLISTED BY JOKA LMAO AND UR BANNED")
+end
+
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
 	ThemeManager.Folder = 'LinoriaLibSettings'
